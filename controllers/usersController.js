8a2d9e80625c280968d9daf17e01efc59d2713e6
ErikMurmi <erikmurmi@gmail.com>
2023-01-08 09:36:30 -0500

    url fix

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,8 @@
+const base_url=process.env.NEXT_PUBLIC_APIKEY
+
 export const deleteUser = async(selectedUser)=>{
     try{
-      await fetch(`http://localhost:3000/api/users/${selectedUser._id}`,{
+      await fetch(`${base_url}/users/${selectedUser._id}`,{
       method:"DELETE",})
     }catch (error){
       console.log(error)
@@ -9,7 +11,7 @@ export const deleteUser = async(selectedUser)=>{
 
 export const addUser = async (newUser)=>{
     try{
-        await fetch('http://localhost:3000/api/users',{
+        await fetch(`${base_url}/users`,{
             method:'POST',
             headers:{
                 "Content-Type":"application/json"
@@ -23,7 +25,7 @@ export const addUser = async (newUser)=>{
   
 export const updateUser = async ({query,newUser})=>{
 try{
-    await fetch('http://localhost:3000/api/users/'+query.id,{
+    await fetch(`${base_url}/users/`+query.id,{
         method:'PUT',
         headers:{
             "Content-Type":"application/json"
@@ -37,7 +39,7 @@ try{
 
 export const getUser = async(query)=>{
     console.log('la query es : ', query )
-    const res = await fetch(`http://localhost:3000/api/users/getById?id=${query.id}`)
+    const res = await fetch(`${base_url}/users/getById?id=${query.id}`)
     const data = await res.json()
     return data   
 }
